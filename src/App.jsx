2ecfd2e9a7872ec52c687d32974a75e0c5aaf16a
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-09-11 10:01:08 +0200

    Added all the needed stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,17 @@ function App() {
 
   return (
     <>
+      <button>Ladda upp</button>
+      <select>
+        <option>Sortera</option>
+        <option>A - Ö</option>
+        <option>Ö - A</option>
+        <option>Stigande filstorlek</option>
+        <option>Fallande filstorlek</option>
+      </select>
+      <div style={{width: "500px", height: "500px", backgroundColor: "gray" }}>
+      <button>Ladda ner</button>
+        </div>
     </>
   )
 }
