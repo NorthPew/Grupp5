a2b71325dc5a6d7f473c54fdf1968ab72b70214e
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-09-12 09:12:02 +0200

    Made dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
+import '../src/app.css'
+
 function App() {
 
   return (
-    <>
-      <button>Ladda upp</button>
-      <select>
-        <option>Sortera</option>
-        <option>A - Ö</option>
-        <option>Ö - A</option>
-        <option>Stigande filstorlek</option>
-        <option>Fallande filstorlek</option>
-      </select>
-      <div style={{width: "500px", height: "500px", backgroundColor: "gray" }}>
-      <button>Ladda ner</button>
-        </div>
-    </>
+    <div className="center-box">
+      <header className='header-container'>
+        <button>Ladda upp</button>
+        <h1>Bildvisare</h1>
+        <select>
+          <option>Sortera</option>
+          <option>A - Ö</option>
+          <option>Ö - A</option>
+          <option>Stigande filstorlek</option>
+          <option>Fallande filstorlek</option>
+        </select>
+      </header>
+    </div>
   )
 }
 
