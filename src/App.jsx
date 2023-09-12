6d085ad6b304ea15bfac47ddcb27ea1e97dcc73e
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-09-12 15:31:51 +0200

    Now you can 'upload' images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
+import { Component } from 'react'
 import '../src/app.css'
 
-function App() {
-  return (
-    <div className="center-box">
+class  App extends Component {
+  state = {
+    selectedFile: null,
+    fileUploadedSuccess: false
+  }
+
+  onFileChange = event => {
+    this.setState({selectedFile: event.target.files[0]})
+  }
+
+  onFileUpload = () => {
+    const formData = new FormData()
+    formData.append(
+    "demo file",
+    this.state.selectedFile
+    );
+
+    // call api
+    console.log('Detta är ett låtsas API POST REQUEST!: ', formData);
+    // this.setState({selectedFile: null, fileUploadedSuccess: true})
+  }
+
+  // Kanske ta bort detta sen
+  fileData = () => {
+    if (this.state.selectedFile) {
+      return (
+        <p>Filnamn: {this.state.selectedFile.name}</p>
+      )
+    } else if (this.state.fileUploadedSuccess) {
+      return (
+        <p>Filen har laddats upp</p>
+      )
+    } else {
+      return (
+        <p>Ladda upp fil!</p>
+      )
+    }
+}
+
+  render() {
+    return (
+      <div className="center-box">
       <header className="header-container">
-        <button className="btn-loading">Ladda upp</button>
+        <input type="file" onChange={this.onFileChange} />
+        <button onClick={this.onFileUpload}>Ladda upp</button>
         <h1>Bildvisare</h1>
         <select className='sort-item'>
           <option>Sortera</option>
@@ -14,18 +55,21 @@ function App() {
           <option>Fallande filstorlek</option>
         </select>
       </header>
-
+  
       <div className="image-container">
           {
+            this.fileData()
             // map function here
-                <div class="image-box">
-                  <a className="download-button" href={url} download target="_blank">Förstora bild och ladda ner</a>
-                  <img src={url} width="200" height="125"></img>
-                </div>
+            //  <div class="image-box">
+            // <a className="download-button" href={url} download target="_blank">Förstora bild och ladda ner</a>
+            // <img src={url} width="200" height="125"></img>
+            // </div>
         }
       </div>
     </div>
-  )
+    )
+
+  }
 }
 
 export default App
