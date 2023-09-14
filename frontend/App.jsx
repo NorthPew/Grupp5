029e0c3830ae7a682b6c371f3f6d9d09669556a0
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-09-14 16:09:12 +0200

    Everything works as planned

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,67 +1,98 @@
-import { Component } from 'react'
+import { Component, useEffect, useState } from 'react'
 import { API_URL } from './constants'
+
+
 import './app.css'
-class App extends Component {
 
-  // States
-  state = {
-    selectedFile: null,
+import AWS from 'aws-sdk';
+
+AWS.config.update({ 
+  accessKeyId: "AKIAQHMIJWQQFB6F7EZC", 
+  secretAccessKey: "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", 
+  region: 'eu-north-1', 
+  bucket: 's3buckergrupp5'
+  }); 
+
+const s3 = new AWS.S3();
+
+
+
+function App() {
+  const [selectedImage, setSelectedImage] = useState(null);;
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    getImages()
+  }, []);
+
+
+  console.log(images);
+
+  function onFileChange(event) {
+   setSelectedImage(event.target.files[0])
   }
-
-  // Upload part
-
-  onFileChange = event => {
-    this.setState({selectedFile: event.target.files[0]})
-  }
-
-  onFileUpload = async () => {
+  
+  async function onFileUpload() {
     const url = `${API_URL}`
-
-  fetch(url, {
-    method: 'POST',
-    mode: 'cors',
-    body: JSON.stringify({key: `${this.state.selectedFile.name}`})
-   })
-
-   .then((res) => res.json())
-   .then((res) => {
-    console.log(res);
-    fetch(res.URL, {
-      method: 'PUT',
+  
+    fetch(url, {
+      method: 'POST',
       mode: 'cors',
-      body: this.state.selectedFile
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err))
-   })
-   .catch((err) => console.log(err))
+      body: JSON.stringify({key: `${selectedImage.name}`})
+     })
+  
+     .then((res) => res.json())
+     .then((res) => {
+      console.log(res);
+      fetch(res.URL, {
+        method: 'PUT',
+        mode: 'cors',
+        body: selectedImage
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+     })
+     .catch((err) => console.log(err))
   }
+  
+  function getImages() {
+    getImages = () => {
+      const params = {
+        Bucket: 's3buckergrupp5',
+      };
+  
+      s3.listObjectsV2(params, (err, data) => {
+        if (err) {
+          console.error('Fel vid hämtning av bilder:', err);
+        } else {
+  
+          const imageList = data.Contents.map((obj) => obj.Key);
+          return setImages(imageList);
+        }
+      });
+    };
+  }
+
 
   // Render
-  render() {
     return (
       <div className="center-box">
       <header className="header-container">
-          <input type="file" onChange={this.onFileChange} accept='image/*' />
-          <button onClick={this.onFileUpload}>Ladda upp</button>
+          <input type="file" onChange={onFileChange} accept='image/*' />
+          <button onClick={onFileUpload}>Ladda upp</button>
 
         <h1>Bildvisare</h1>
       </header>
   
       <div className="image-container">
         {
-          /* this.state.images.map(item => {
-            <div key={item} class="image-box">
-              <a className="download-button" href={item} download target="_blank">Förstora bild och ladda ner</a>
-              <img src={item} width="200" height="125"></img>
-             </div>
-          }) */
+          images.map((image, index) => {
+            <li key={image}><img src={`http://s3buckergrupp5.s3-website.eu-north-1.amazonaws.com/${image}`} alt={`Image ${index}`} /></li>
+          })
         }
       </div>
     </div>
     )
-
-  }
 }
 
 export default App
