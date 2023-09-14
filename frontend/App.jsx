b4e2d6cc5b5a5300ff7d3e439065ec7ce821bb37
Victor (Vigge) B <vbergstrom02@gmail.com>
2023-09-14 12:15:11 +0200

    Update

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,13 +1,23 @@
 import { Component } from 'react'
 import { API_URL } from './constants'
-
+import AWS from 'aws-sdk';
 import './app.css'
+
+AWS.config.update({ 
+  accessKeyId: '', 
+  secretAccessKey: '', 
+  region: 'eu-north-1', 
+  }); 
+
+
+const s3 = new AWS.S3();
 
 class App extends Component {
 
   // States
   state = {
     selectedFile: null,
+    images: []
   }
 
   // Upload part
@@ -19,10 +29,10 @@ class App extends Component {
   onFileUpload = async () => {
     const url = `${API_URL}`
 
-   await fetch(url, {
+  fetch(url, {
     method: 'POST',
     mode: 'cors',
-    body: JSON.stringify({key: this.state.selectedFile})
+    body: JSON.stringify({key: `${this.state.selectedFile.name}`})
    })
 
    .then((res) => res.json())
@@ -36,8 +46,24 @@ class App extends Component {
     .then((res) => console.log(res))
     .catch((err) => console.log(err))
    })
-
    .catch((err) => console.log(err))
+  }
+
+  getImages() {
+    const params = {
+      Bucket: 's3buckergrupp5'
+    }
+
+    s3.listObjectsV2(params, (err, data) => {
+      if (err) {
+        console.error('Fel vid hämtning av bilder:', err);
+      } else {
+
+        const imageList = data.Contents.map((obj) => obj.Key);
+        this.setState({images: [imageList]});
+      }
+    });
+
   }
 
 
@@ -50,23 +76,16 @@ class App extends Component {
           <button onClick={this.onFileUpload}>Ladda upp</button>
 
         <h1>Bildvisare</h1>
-        <select className='sort-item'>
-          <option>Sortera</option>
-          <option>A - Ö</option>
-          <option>Ö - A</option>
-          <option>Stigande filstorlek</option>
-          <option>Fallande filstorlek</option>
-        </select>
       </header>
   
       <div className="image-container">
         {
-          //data.map(item => {
-          //  <div key={item} class="image-box">
-          //    <a className="download-button" href={item} download target="_blank">Förstora bild och ladda ner</a>
-          //    <img src={item} width="200" height="125"></img>
-          //   </div>
-          //})
+          this.state.images.map(item => {
+            <div key={item} class="image-box">
+              <a className="download-button" href={item} download target="_blank">Förstora bild och ladda ner</a>
+              <img src={item} width="200" height="125"></img>
+             </div>
+          })
         }
       </div>
     </div>
--- frontend/constants.js
+++ frontend/constants.js
@@ -1,2 +1,2 @@
 // Denna måste vi ändra på när vi lägger upp projektet på render
-export const API_URL = 'http://localhost:666'; 
+export const API_URL = 'https://otj4u6rcv0.execute-api.eu-north-1.amazonaws.com/sample/sampleuploadfunction'; 
